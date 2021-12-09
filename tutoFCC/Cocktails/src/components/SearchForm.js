94e0a774dbdd15a34374209fa22da2b6e6712178
kevin calderon <kncalderong@gmail.com>
2021-12-08 22:49:31 -0500

    cocktails route project finished

---
+++ tutoFCC/Cocktails/src/components/SearchForm.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useGlobalContext } from "../context";
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+
+  //to focus immediately on search bar
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = (e) => {
+    setSearchTerm(searchValue.current.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">Search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
---
+++ tutoFCC/Cocktails/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, quis.
+        Quaerat dolore rerum distinctio nostrum similique minus odio corporis
+        necessitatibus unde. Rem, assumenda modi maiores velit beatae sunt? Quod
+        doloremque praesentium recusandae, veniam cumque magni mollitia quasi
+        voluptas cupiditate excepturi.
+      </p>
+    </section>
+  );
+};
+
+export default About;
