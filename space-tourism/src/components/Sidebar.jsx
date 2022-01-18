2dbf09ae0c4a3a64ead6fe7942b527c35c83dedd
kevin calderon <kncalderong@gmail.com>
2022-01-17 20:35:07 -0500

    toggle button fixed, home mobile layout progress1.0

--- space-tourism/src/components/Sidebar.jsx
+++ space-tourism/src/components/Sidebar.jsx
@@ -5,15 +5,15 @@ import { MdClose } from "react-icons/md";
 
 const Sidebar = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
-  console.log(toggleSidebar);
+
   return (
-    <div className="flex md:flex-row flex-col  transition-height duration-75 ease-out">
+    <div className=" w-full p-4 relative">
       {/*This is on the big screen */}
       <div className="hidden sm:flex ">This is the navbar on bigscreen</div>
 
       {/*This is on small screen */}
-      <div className="flex sm:hidden flex-col relative">
-        <nav className="w-full p-5 flex justify-between ">
+      <div className="flex sm:hidden flex-col relative w-full">
+        <nav className=" p-5 flex justify-between  ">
           <img src={starLogo} alt="star-logo" className=" w-[40px] h-[40px]" />
           <HiMenu
             fontSize={40}
@@ -21,20 +21,25 @@ const Sidebar = () => {
             onClick={() => {
               setToggleSidebar(!toggleSidebar);
             }}
+            color="white"
           />
         </nav>
-        {toggleSidebar && (
-          <div className="absolute right-0 fixed w-2/3 bg-slate-400 h-screen z-10 animate-slide-in transition-all transition-duration-1000 ease-in-out">
-            <div className="flex flex-row justify-end w-full p-5">
-              <MdClose
-                fontSize={40}
-                style={{ color: "#d0d6f9" }}
-                className="cursor-pointer"
-                onClick={() => setToggleSidebar(false)}
-              />
-            </div>
-          </div>
-        )}
+      </div>
+      <div
+        className="absolute right-0 top-0 fixed bg-slate-400 w-2/3 min-h-[667px] z-10 transition-all duration-200 ease-in p-4"
+        style={{
+          width: !toggleSidebar ? "0" : "66%",
+          visibility: !toggleSidebar ? "hidden" : "visible",
+        }}
+      >
+        <div className="flex flex-row justify-end w-full p-5">
+          <MdClose
+            fontSize={40}
+            style={{ color: "#d0d6f9" }}
+            className="cursor-pointer"
+            onClick={() => setToggleSidebar(false)}
+          />
+        </div>
       </div>
     </div>
   );
--- space-tourism/src/pages/Home.jsx
+++ space-tourism/src/pages/Home.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import Sidebar from "../components/Sidebar";
 
 const Home = () => {
+  //../assets/home/background-home-mobile.jpg
+
   return (
-    <div>
-      <Sidebar />
-      Home
+    <div className="min-h-[667px] w-full min-w-[375px]  bg-[url('../public/home/background-home-mobile.jpg')]">
+      <Sidebar className="animate-slide-in" />
+      <div className="w-full px-8">
+        <p className="navText text-center text-spaceMiddle">
+          so you want to travel to
+        </p>
+        <p className="heading-2 text-center ">space</p>
+        <p className="regularText text-center text-spaceMiddle">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
     </div>
   );
 };
