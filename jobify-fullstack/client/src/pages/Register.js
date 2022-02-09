c69a7d01c6235d4f690e8d4fd0c622803aa8a871
kevin calderon <kncalderong@gmail.com>
2022-02-08 21:38:15 -0500

    register page finished

--- jobify-fullstack/client/src/pages/Register.js
+++ jobify-fullstack/client/src/pages/Register.js
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Logo, FormRow, Alert } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+  showAlert: true,
+};
 
 const Register = () => {
-  return <h1>Register</h1>;
+  const [values, setValues] = useState(initialState);
+
+  //global state and useNavigate
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form onSubmit={onSubmit} className="form">
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {values.showAlert && <Alert />}
+        {!values.isMember && (
+          <FormRow
+            name="name"
+            type="text"
+            handleChange={handleChange}
+            value={values.name}
+          />
+        )}
+        <FormRow
+          name="email"
+          type="email"
+          handleChange={handleChange}
+          value={values.email}
+        />
+        <FormRow
+          name="password"
+          type="password"
+          handleChange={handleChange}
+          value={values.password}
+        />
+
+        <button className="btn btn-block" type="submit">
+          submit
+        </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button className="member-btn" type="button" onClick={toggleMember}>
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
