385774349db8442ddaa7d1298c1d4bd6070764c3
kevin calderon <kncalderong@gmail.com>
2022-02-09 16:58:49 -0500

    reducer setUp finished

--- jobify-fullstack/client/src/components/Alert.js
+++ jobify-fullstack/client/src/components/Alert.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useAppContext } from "../context/appContext";
 
 const Alert = () => {
-  return <div className="alert alert-danger">Alert goes here</div>;
+  const { alertType, alertText } = useAppContext(); //this access to the values(that usually are states and functions on the global context)
+  return <div className={`alert alert-${alertType}`}>{alertText}</div>;
 };
 
 export default Alert;
--- jobify-fullstack/client/src/pages/Register.js
+++ jobify-fullstack/client/src/pages/Register.js
@@ -1,27 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { Logo, FormRow, Alert } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
+import { useAppContext } from "../context/appContext";
 
 const initialState = {
   name: "",
   email: "",
   password: "",
   isMember: true,
-  showAlert: true,
 };
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
+  const { showAlert, isLoading, displayAlert } = useAppContext();
 
   //global state and useNavigate
 
   const handleChange = (e) => {
-    console.log(e.target);
+    setValues({ ...values, [e.target.name]: e.target.value });
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    const { email, name, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert();
+      return;
+    }
   };
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
@@ -32,7 +37,7 @@ const Register = () => {
       <form onSubmit={onSubmit} className="form">
         <Logo />
         <h3>{values.isMember ? "Login" : "Register"}</h3>
-        {values.showAlert && <Alert />}
+        {showAlert && <Alert />}
         {!values.isMember && (
           <FormRow
             name="name"
