8d3abb0f421fc08b3767248afb301b2e785d1134
kevin calderon <kncalderong@gmail.com>
2022-02-15 21:54:56 -0500

    dashboard nested routes done, and home page surrounded by a protected component

--- jobify-fullstack/client/src/App.js
+++ jobify-fullstack/client/src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { Landing, Register, Error, Dashboard } from "./pages";
+import { Landing, Register, Error, ProtectedRoute } from "./pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from "./pages/dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          {/*index here is showing that is the default showed page*/}
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />}></Route>
+          <Route path="add-job" element={<AddJob />}></Route>
+          <Route path="profile" element={<Profile />}></Route>
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
