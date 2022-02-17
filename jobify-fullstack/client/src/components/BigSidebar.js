10de22a3257060c7c6e20b10ea2d29eb7dec728a
kevin calderon <kncalderong@gmail.com>
2022-02-16 22:45:45 -0500

    big sidebar finished

--- jobify-fullstack/client/src/components/BigSidebar.js
+++ jobify-fullstack/client/src/components/BigSidebar.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/BigSidebar";
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const BigSidebar = () => {
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
-      <div>BigSidebar</div>
+      <div className={`sidebar-container ${showSidebar && "show-sidebar"}`}>
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
--- jobify-fullstack/client/src/components/SmallSidebar.js
+++ jobify-fullstack/client/src/components/SmallSidebar.js
@@ -2,6 +2,7 @@ import React from "react";
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import { FaTimes } from "react-icons/fa";
 import { useAppContext } from "../context/appContext";
+
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 
