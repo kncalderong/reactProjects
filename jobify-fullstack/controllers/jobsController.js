35b170fcabc41489c383db5f7050a496b7db4ac4
kevin calderon <kncalderong@gmail.com>
2022-02-19 20:06:26 -0500

    create job backend finished

--- jobify-fullstack/controllers/jobsController.js
+++ jobify-fullstack/controllers/jobsController.js
@@ -1,5 +1,24 @@
-const createJob = async (req, res) => {
-  res.send("createJob");
+import Job from "../models/Job.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
+
+const createJob = async (req, res, next) => {
+  try {
+    const { position, company } = req.body;
+
+    if (!position || !company) {
+      throw new BadRequestError("Please provide all values");
+    }
+
+    //to include the Id referred to whom created the job
+    req.body.createdBy = req.user.userId;
+    console.log(req.user);
+
+    const job = await Job.create(req.body);
+    res.status(StatusCodes.CREATED).json({ job });
+  } catch (error) {
+    next(error);
+  }
 };
 const deleteJob = async (req, res) => {
   res.send("deleteJob");
