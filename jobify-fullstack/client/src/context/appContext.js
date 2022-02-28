74280b266bf0e76488de96912ec37fa1e703722b
kevin calderon <kncalderong@gmail.com>
2022-02-27 20:08:49 -0500

    pagination buttons and functionallity front end finished

--- jobify-fullstack/client/src/context/appContext.js
+++ jobify-fullstack/client/src/context/appContext.js
@@ -27,6 +27,7 @@ import {
   SHOW_STATS_BEGIN,
   SHOW_STATS_SUCCESS,
   CLEAR_FILTERS,
+  CHANGE_PAGE,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -224,8 +225,8 @@ const AppProvider = ({ children }) => {
     clearAlert();
   };
   const getJobs = async () => {
-    const { search, searchType, searchStatus, sort } = state;
-    let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}`;
+    const { search, searchType, searchStatus, sort, page } = state;
+    let url = `/jobs?page=${page}&status=${searchStatus}&jobType=${searchType}&sort=${sort}`;
     if (search) {
       url = url + `&search=${search}`;
     }
@@ -303,6 +304,9 @@ const AppProvider = ({ children }) => {
   const clearFilters = () => {
     dispatch({ type: CLEAR_FILTERS });
   };
+  const changePage = (page) => {
+    dispatch({ type: CHANGE_PAGE, payload: { page } });
+  };
   return (
     <AppContext.Provider
       value={{
@@ -321,6 +325,7 @@ const AppProvider = ({ children }) => {
         editJob,
         showStats,
         clearFilters,
+        changePage,
       }}
     >
       {children}
--- jobify-fullstack/client/src/context/reducer.js
+++ jobify-fullstack/client/src/context/reducer.js
@@ -24,6 +24,7 @@ import {
   SHOW_STATS_SUCCESS,
   SHOW_STATS_BEGIN,
   CLEAR_FILTERS,
+  CHANGE_PAGE,
 } from "./actions";
 import { initialState } from "./appContext";
 
@@ -108,7 +109,7 @@ const reducer = (state, action) => {
     };
   }
   if (action.type === HANDLE_CHANGE) {
-    return { ...state, [action.payload.name]: action.payload.value };
+    return { ...state, page: 1, [action.payload.name]: action.payload.value };
   }
   if (action.type === CLEAR_VALUES) {
     const initialState = {
@@ -212,6 +213,9 @@ const reducer = (state, action) => {
       searchType: "all",
       sort: "latest",
     };
+  }
+  if (action.type === CHANGE_PAGE) {
+    return { ...state, page: action.payload.page };
   }
   throw new Error(`no such action: ${action.type}`);
 };
