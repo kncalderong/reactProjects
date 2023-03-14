1157a03aa08cc10de0465bd03fba1b5d3f6e899b
Kevin Nicolas <kncalderong@gmail.com>
2023-03-14 17:39:19 -0500

    feat: cart container section build

--- redux-toolkit/src/App.js
+++ redux-toolkit/src/App.js
@@ -1,10 +1,12 @@
+import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
 
 
 
 function App() {
   return <main>
-    <Navbar/>
+    <Navbar />
+    <CartContainer/>
   </main>;
 }
 export default App;
