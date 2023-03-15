a109b2c6bae545092b1f4260870bbfc3c47a9ace
Kevin Nicolas <kncalderong@gmail.com>
2023-03-15 11:05:06 -0500

    feat: calculate totals reducer included

--- redux-toolkit/src/App.js
+++ redux-toolkit/src/App.js
@@ -1,9 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
-
-
+import { useEffect } from "react";
+import { calculateTotals } from "./features/cart/cartSlice";
 
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+    // eslint-disable-next-line
+  }, [cartItems]);
+  
   return <main>
     <Navbar />
     <CartContainer/>
